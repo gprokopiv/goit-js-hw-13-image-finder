8cd579694c1994bc5aeae152b9e6506186aefe42
Prokopiv <halyna.kotlyar@gmail.com>
2021-01-25 03:28:10 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -13,58 +13,28 @@ function onSearch(e) {
     apiService.query = e.currentTarget.elements.query.value;
   
     if (apiService.query === '') {
-      return alert('Введи что-то нормальное');
+      return alert('sorry');
     }
-    loadMoreBtn.show();
+    // loadMoreBtn.show();
     apiService.resetPage();
   clearGalleryContainer();
   fetchImages();
 }
 function fetchImages() {
-    loadMoreBtn.disable();
+    // loadMoreBtn.disable();
     apiService.fetchImages().then(images => {
         appendImageMarkup(images);
-      loadMoreBtn.enable();
+    //   loadMoreBtn.enable();
     });
   }
-  
   function appendImageMarkup(images) {
     const markup = galleryItemTpl(images);
     refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
 }
+ 
   
   function clearGalleryContainer() {
     refs.galleryContainer.innerHTML = '';
   }
 
-  galleryContainer.addEventListener('click', onClick);
   
-  function onClick(event) {
-    event.preventDefault()
-
-    if (event.target.nodeName !=='IMG') {
-      return;
-    }
-
-    modalImage.src = event.target.dataset.source;
-    modalBox.classList.add('is-open');
-    window.addEventListener('keydown', onEscapePress);
-
-  };
- 
-  function onEscapePress(event) {
-    if (event.code === 'Escape') {
-      onCloseModalBtn();
-    }
-  }
-   
-
-  const closeModalBtn = document.querySelector(
-    '[data-action="close-lightbox"]',
-  );
-  closeModalBtn.addEventListener('click', onCloseModalBtn);
-  function onCloseModalBtn() {
-    window.removeEventListener('keydown', onEscapePress);
-    modalBox.classList.remove('is-open');
-  };
-   
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,42 +1,39 @@
+import getRefs from '../js/get-refs';
+import galleryItemTpl from '../templates/gallery-item.hbs';
+
+const refs = getRefs();
 const API_KEY = '20009503-42c59acfee619c94b9f5dbeeb';
 const BASE_URL = 'https://pixabay.com/api/';
-// function fetchImages(imageId) {
-//   return fetch(`${BASE_URL}${imageId}`).then(response =>
-//     response.json(),
-//   );
-// }
-// export default { fetchImages };
 
-export default class apiService {
-  constructor() {
-    this.searchQuery = '';
-    this.page = 1;
-  }
+export default {
+  searchQuery: '',
+  page: 1,
 
   fetchImages() {
     const url = `${BASE_URL}/image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
-
     return fetch(url)
       .then(response => response.json())
-      .then(({ imageId }) => {
+      .then(( images ) => {
+        const markup = galleryItemTpl(images);
+        refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
         this.incrementPage();
-        return imageId;
+        return images;
       });
-  }
+  },
 
   incrementPage() {
     this.page += 1;
-  }
+  },
 
   resetPage() {
     this.page = 1;
-  }
+  },
 
   get query() {
     return this.searchQuery;
-  }
+  },
 
   set query(newQuery) {
     this.searchQuery = newQuery;
-  }
-}
+  },
+};
